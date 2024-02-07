8e111d4b9b56aa575e4bbdce16bc0fffd4e250ad
Jean Andrade <jean_andrade1@icloud.com>
2024-02-06 23:14:46 -0300

    feat: Implementing drive and controllers with lib handlers

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
@@ -7,7 +8,11 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
         body = http_request.body
         product_code = body["product_code"]
+
+        tag_creator_controller.create(product_code)
+        
 
         return HttpResponse(status_code=200, body={"Hello": "world"})
